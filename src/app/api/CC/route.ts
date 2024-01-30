38f4614b4b58c1ca5a578837a41c3cf1c749528a
7Ebra <creator@cyberwaifu.org>
2024-01-30 22:03:15 +0800

    20240130.2
    Add CC options

--- src/app/api/CC/route.ts
+++ src/app/api/CC/route.ts
@@ -8,6 +8,11 @@ export async function POST(request: Request) {
   const data = await request.formData();
   const card = data.get("card");
   const ccselect = data.get("ccselect");
+  let ccauthor = data.get("ccauthor");
+  let ccfilename = data.get("ccfilename")
+  let ccmes = data.get("ccmes");
+  let cctitleimg = data.get("cctitleimg");
+  let ccbgimg = data.get("ccbgimg");
   let CC = "CC BY-NC 4.0";
   let CCweb = "https://creativecommons.org/licenses/by-nc/4.0/deed.zh-hans";
   
@@ -30,9 +35,20 @@ export async function POST(request: Request) {
     CC = "CC BY-SA 4.0";
     CCweb = "https://creativecommons.org/licenses/by-sa/4.0/legalcode.zh-hans";
   }
+
+  let ccimg = "";
+  let ccbg = "background: linear-gradient(to bottom, #232526, #414345);";
+  if (cctitleimg !== "") {
+    ccimg = "<img src=" + cctitleimg + " width=30% />";
+  }
   
+  if (ccbgimg !== "") {
+    ccbg = "background-image:url('" + ccbgimg + "');";
+  }
+
   //Edit FirstMes
   if (card instanceof File) {
+
     const buffer = await card.arrayBuffer();
     const cardBuffer = Buffer.from(buffer);
     const chunks = extract(cardBuffer);
@@ -48,7 +64,7 @@ export async function POST(request: Request) {
     const decodedCharaText = new TextDecoder("utf-8").decode(Buffer.from(atob(charaTextBase64), 'binary'));
     const charaData = JSON.parse(decodedCharaText);
     charaData.data.alternate_greetings.unshift(charaData.data.first_mes);
-    charaData.data.first_mes = "<div style='text-align:center; background: linear-gradient(to right, #C9D6FF, #E2E2E2); background-size: cover;'><hr/><h1>{{char}}</h1><h3 style='margin-top:24px'>本内容依据“"+CC+"”许可证进行授权：<a href='"+CCweb+"'>点击查阅该许可证</a></h3><h3 style='text-align: right;margin-top: 48px'>点击右箭头开始正文内容→&nbsp; &nbsp; </h3><hr/></div>";
+    charaData.data.first_mes = "<div style='text-align:center;"+ccbg+"background-size: cover;'><hr/>"+ccimg+"<h1>"+ccfilename+"</h1><h3 style='margin-top:24px'>本内容依据“"+CC+"”许可证进行授权：<a href='"+CCweb+"'>点击查阅该许可证</a></h3><h4 style='margin-top:24px'>作者"+ccauthor+"备注:"+ccmes+"</h4><h3 style='text-align: right;margin-top: 48px'>点击右箭头开始正文内容→&nbsp; &nbsp; </h3><hr/></div>";
     const updatedCharaDataJSON = JSON.stringify(charaData);
     const updatedCharaDataBase64 = Buffer.from(updatedCharaDataJSON, "utf8").toString("base64");
     for (let tEXtChunk of tEXtChunks) {
