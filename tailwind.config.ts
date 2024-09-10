d9594fc69c4abffaf3b5d86840328f9f5386952d
OoCTalk <dev@ooctalk.com>
2024-09-10 21:01:18 +0800

    v2
    bootstrap

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,45 +1,19 @@
-import type { Config } from 'tailwindcss'
-import {nextui} from "@nextui-org/react";
+import type { Config } from "tailwindcss";
+
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
-  darkMode: "class",
-  plugins: [
-    nextui({
-      prefix: "nextui", // prefix for themes variables
-      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-      defaultTheme: "light", // default theme from the themes object
-      defaultExtendTheme: "light", // default theme to extend on custom themes
-      layout: {}, // common layout tokens (applied to all themes)
-      themes: {
-        light: {
-          layout: {}, // light theme layout tokens
-          colors: {
-            background:"#f9fafb"
-          }, // light theme colors
-        },
-        dark: {
-          layout: {}, // dark theme layout tokens
-          colors: {
-            background:"#111827"
-          }, // dark theme colors
-        },
-        // ... custom themes
-      },
-    }),
-  ],
-}
-export default config
+  plugins: [],
+};
+export default config;
