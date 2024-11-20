258e1602d71b90c428f835262a5e5c365c797db2
OoCTalk <dev@ooctalk.com>
2024-11-20 10:30:50 +0800

    2.0.0-pre.2024-11-20.2

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "../globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -8,12 +6,6 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "../../i18n/routing";
-
-// Mi-Sans
-const MiSans = localFont({
-  src: "./_fonts/MiSans VF.ttf",
-  variable: "--font-geist-sans",
-});
 
 export async function generateMetadata({
   params: { locale },
@@ -46,7 +38,7 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <body
-        className={`${MiSans.className} antialiased bg-zinc-100 dark:bg-zinc-900 lg:dark:bg-zinc-950 text-black dark:text-white`}
+        className={`antialiased bg-zinc-100 dark:bg-zinc-900 lg:dark:bg-zinc-950 text-black dark:text-white`}
       >
         <NextIntlClientProvider messages={messages}>
           {children}
